4585390e184012186b297242a8b22dcbd818c8a4
Venugopal K <venu.shastri@gmail.com>
2025-08-11 10:36:36 +0530

    Update misaligned.hpp

--- misaligned.hpp
+++ misaligned.hpp
@@ -5,11 +5,7 @@
 #include <string>
 #include <tuple>
 
-// Generate the color mapping data
-std::vector<std::tuple<int, std::string, std::string>> generateColorMap();
 
-// Format a single color map entry as a string
-std::string formatColorMapEntry(int index, const std::string& major, const std::string& minor);
 
 // Print the color map using a provided output function (dependency injection for testability)
 int printColorMap(const std::function<void(const std::string&)>& outputFunc);
