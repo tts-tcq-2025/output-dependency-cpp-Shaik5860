7079b28b21e62e7d4ee592ff63fb22a74fddc253
Shaik5860 <waseemahammad.shaik@gmail.com>
2025-08-11 10:32:23 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -19,9 +19,6 @@ std::string formatColorMapEntry(int index, const std::string& major, const std::
     return std::to_string(index) + " | " + major + " | " + minor;
 }
 
-void printOnConsole(std::string& lineContent){
-      std::cout<<lineContent<<std::endl;
-}
 int printColorMap(const std::function<void(const std::string&)>& outputFunc) {
     auto colorMap = generateColorMap();
     for (const auto& [index, major, minor] : colorMap) {
@@ -30,38 +27,12 @@ int printColorMap(const std::function<void(const std::string&)>& outputFunc) {
     return static_cast<int>(colorMap.size());
 }
 
-std::vector<std::tuple<int, std::string, std::string>> generateExpectedLines() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
-    std::vector<std::tuple<int, std::string, std::string>> expectedLines;
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++) {
-            expected.push_back(formatColorMapEntry(i * 5 + j, majorColor[i], minorColor[j]));
-        }
-    }
-    return expectedLines;
-}
-/* TEST Environment */
-vector<string&> capturedLines;
-void fakeOutputFunc(std::string& lineContent){
-    capturedLines.push_back(lineContent);
-}
-void testPrintColorMap() {
-    std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap(fakeOutputFunc); 
-    assert(result == 25);//value based test
-    // Content-based test — should FAIL because of the bug
-    auto expectedLines = generateExpectedLines();
-    assert(capturedLines==expectedLines);
-    std::cout << "All is well (maybe!)\n";
-}
-
 
 int main() {
     testPrintColorMap();
     return 0;
 }
+
 
 
 
